b96134331d16c80c9f5361a70d5df967a769fa98
Aarjav Jain <aarjavjain2309@gmail.com>
2023-10-02 02:16:43 +0530

    intial commit

---
+++ src/components/DropBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const DropBox = () => {
+  return (
+    <div className="upload-container">
+      <div className="drop-zone">
+        <div className="icon-container"></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/webShare.png";
+
+export const Navbar = () => {
+  return (
+    <div class="navbar">
+      <img src={logo} alt="webShare" style={{ width: 250, height: 100 }} />
+    </div>
+  );
+};
